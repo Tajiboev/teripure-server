4780b6d892b48377d8a06e3a0a18338e9b5914e1
Mukhammadjon Tajiboev <mukhammadjon.tajiboev@gmail.com>
2022-07-31 23:54:53 +0500

    hope2

--- source/config.ts
+++ source/config.ts
@@ -1,11 +1,9 @@
-import dotenv from 'dotenv';
+const MONGO_USERNAME = process.env.MONGO_USERNAME;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+const SECRET_TOKEN = process.env.SECRET_TOKEN;
 
-const config = dotenv.config();
-if (config.error || !config.parsed) throw config.error;
-const env = config.parsed;
-
-const dbUrl = `mongodb+srv://${env.MONGO_USERNAME}:${env.MONGO_PASSWORD}@cluster0.kszv0zh.mongodb.net/?retryWrites=true&w=majority`;
-const port = env.PORT;
-const secret_token = env.SECRET_TOKEN;
+const dbUrl = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.kszv0zh.mongodb.net/?retryWrites=true&w=majority`;
+const port = process.env.PORT || 3000;
+const secret_token = SECRET_TOKEN || 'ultrasafe secter';
 
 export { dbUrl, port, secret_token };
