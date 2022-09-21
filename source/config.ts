1b7a84e971ab7da44762561e5e8b6b581fcac4d1
Mukhammadjon Tajiboev <mukhammadjon.tajiboev@gmail.com>
2022-09-21 18:58:52 +0500

    fix

--- source/config.ts
+++ source/config.ts
@@ -1,11 +1,10 @@
-const MONGO_USERNAME = process.env.MONGO_USERNAME;
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-const SECRET_TOKEN = process.env.SECRET_TOKEN;
+const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
 const BOT_TOKEN = process.env.BOT_TOKEN || '';
 const port = process.env.PORT || 5000;
-const secret_token = process.env.SECRET_TOKEN;
+const secret_token = process.env.SECRET_TOKEN || '';
 const secret_key = process.env.CLICK_SECRET_KEY || '';
-const merchant_user_id = process.env.CLICK_MERCHANT_USER_ID;
+const merchant_user_id = process.env.CLICK_MERCHANT_USER_ID || '';
 const dbUrl = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.kszv0zh.mongodb.net/?retryWrites=true&w=majority`;
 
 export { dbUrl, port, secret_token, BOT_TOKEN, secret_key, merchant_user_id };
